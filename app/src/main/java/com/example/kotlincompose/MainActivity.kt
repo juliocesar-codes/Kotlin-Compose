d9e3538eaefacd866d6f6e8c727cf01179616e7f
juliocesar-codes <juliocesarsantanaalves26@gmail.com>
2026-02-23 14:30:21 -0300

    Update

--- app/src/main/java/com/example/kotlincompose/MainActivity.kt
+++ app/src/main/java/com/example/kotlincompose/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlincompose.ui.theme.KotlinComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,10 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-    Column() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = "",
-            onValueChange = {}
+            onValueChange = {},
+            placeholder = {
+                Text("O que você está procurando?")
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.outline_document_search_24),
+                    contentDescription = "Icone de Pesquisa"
+                )
+            }
         )
     }
 }
